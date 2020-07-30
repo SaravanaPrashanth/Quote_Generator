c5c037d97b75b88188eacbecd18053a6a29f48af
Saravana Prashanth <sakthisakthi2000@gmail.com>
2020-07-30 13:40:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// GET Quote from API
+async function getQuote(){
+    showLoadingSpinner();
+
+    const proxyURL = 'https://jacinto-cors-proxy.herokuapp.com/';
+    const apiURL = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown';
+        }else{
+            authorText.innerText = data.quoteAuthor;
+        }
+        if(data.quoteText.length> 120){
+            quoteText.classList.add('long-quote');
+        }else{
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+
+        removeLoadingSpinner();
+    } catch (error) {
+        getQuote();
+    }
+}
+
+// Tweet Function
+function tweetQuote()
+{
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterURL, '_blank');
+}
+// Event Listener
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// OnLoad
+getQuote();
